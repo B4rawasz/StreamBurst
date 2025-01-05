7ed7277c812e30b93e1047a26ca817048c590bc8
Barawasz <69699459+B4rawasz@users.noreply.github.com>
2025-01-05 04:25:36 +0100

    add module testing functionality in main process

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,6 +1,7 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
 import { isDev } from "./utils.js";
+import { pathToFileURL } from "url";
 
 app.on("ready", () => {
 	const mainWindow = new BrowserWindow({});
@@ -11,4 +12,37 @@ app.on("ready", () => {
 			path.join(app.getAppPath(), "dist-react", "index.html")
 		);
 	}
+
+	if (isDev()) {
+		testModules();
+	}
 });
+
+async function testModules() {
+	const modulePath = path.join(
+		app.getAppPath(),
+		"modules",
+		"test.asar",
+		"index.js"
+	);
+	console.log("modulePath", modulePath);
+
+	const testMod = await import(pathToFileURL(modulePath).href)
+		.then((mod) => {
+			return mod.default;
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+
+	testMod.on("event", (data: any) => {
+		console.log("event", data);
+	});
+
+	testMod.enable();
+
+	// Example: Disable the module after 10 seconds
+	setTimeout(() => {
+		testMod.disable();
+	}, 10000);
+}
