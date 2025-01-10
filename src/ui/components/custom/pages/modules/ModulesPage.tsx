ebac099cae5c2c2b10a52cde0dcdf594f3554513
Barawasz <69699459+B4rawasz@users.noreply.github.com>
2025-01-10 01:04:08 +0100

    Remove unused Test component from ModulesPage

--- src/ui/components/custom/pages/modules/ModulesPage.tsx
+++ src/ui/components/custom/pages/modules/ModulesPage.tsx
@@ -28,32 +28,6 @@ import { DialogClose } from "@radix-ui/react-dialog";
 import { Puzzle, Settings } from "lucide-react";
 import { useEffect, useState } from "react";
 
-const Test = () => {
-	return (
-		<Card className=" flex-grow flex flex-col">
-			<CardHeader>
-				<CardTitle>CS2</CardTitle>
-				<CardDescription>Lorem ipsum.</CardDescription>
-			</CardHeader>
-			<CardContent className="flex-grow flex">
-				<ScrollArea className="max-h-24 shrink">
-					<span>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at
-						condimentum est. Curabitur varius ligula nibh, scelerisque feugiat
-						risus mattis quis. Vivamus consectetur condimentum eleifend. Aliquam
-						iaculis velit quis ligula varius, sed placerat nisl interdum.
-						Suspendisse potenti. Nulla ornare magna libero, ac maximus tellus
-						dictum sit amet.
-					</span>
-				</ScrollArea>
-			</CardContent>
-			<CardFooter className="flex justify-end">
-				<Switch id="cs2" />
-			</CardFooter>
-		</Card>
-	);
-};
-
 const ModulesPage = () => {
 	const modules = getModules();
 
