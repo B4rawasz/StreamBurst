5c7adab0dba6ebe584cba209ed44af92abf31752
Barawasz <69699459+B4rawasz@users.noreply.github.com>
2025-03-15 23:57:29 +0100

    Update basePath in config.mjs for production environment

--- docs/src/config/config.mjs
+++ docs/src/config/config.mjs
@@ -1,4 +1,4 @@
 export const basePath =
-	process.env.NODE_ENV == "development" ? "" : "/testPages";
+	process.env.NODE_ENV == "development" ? "" : "/StreamBurst";
 
 export const isDev = process.env.NODE_ENV == "development";
