d3afdf93d1b8397c6b4bfa656c7b771a123bb221
Barawasz <69699459+B4rawasz@users.noreply.github.com>
2025-03-16 01:27:16 +0100

    Update documentation link in layout component

--- docs/src/app/(home)/layout.tsx
+++ docs/src/app/(home)/layout.tsx
@@ -9,7 +9,7 @@ export default function Layout({ children }: { children: ReactNode }) {
 			links={[
 				{
 					text: "Documentation",
-					url: "/docs/test1",
+					url: "/docs/app",
 					active: "nested-url",
 				},
 			]}
