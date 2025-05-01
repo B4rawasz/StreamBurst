45bad056b93c2ddd261df04547484aa931253986
Barawasz <69699459+B4rawasz@users.noreply.github.com>
2025-05-01 12:37:37 +0200

    Clean up

--- src/ui/components/custom/pages/editor/EditorPage.tsx
+++ src/ui/components/custom/pages/editor/EditorPage.tsx
@@ -1,12 +1,6 @@
-import { Button } from "@/components/ui/button";
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
-import { ScrollArea } from "@/components/ui/scroll-area";
 import { PencilRuler } from "lucide-react";
-import { useEffect, useState } from "react";
 
 const EditorPage = () => {
-	const pages = getPages();
-
 	return (
 		<div className="h-full w-full flex flex-col">
 			<span className="text-4xl font-bold flex items-center gap-4 p-4">
@@ -30,30 +24,5 @@ const EditorPage = () => {
 		</div>
 	);
 };
-
-const PageCard = ({ pageName }: { pageName: string }) => {
-	return (
-		<Card>
-			<CardHeader className="py-2 pr-2">
-				<CardTitle className="flex flex-row justify-between items-center">
-					{pageName}
-					<Button className="m-0 mt-0">Edit</Button>
-				</CardTitle>
-			</CardHeader>
-		</Card>
-	);
-};
-
-function getPages() {
-	const [pages, setPages] = useState<string[] | null>(null);
-
-	useEffect(() => {
-		(async () => {
-			setPages(await window.electron.getPages());
-		})();
-	}, []);
-
-	return pages;
-}
 
 export default EditorPage;
