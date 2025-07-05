7266a1989171591d1f19c20d45cba3a38536fc55
Barawasz <69699459+B4rawasz@users.noreply.github.com>
2025-07-05 13:15:05 +0200

    Add NavigationMenu

--- docs/src/app/(home)/layout.tsx
+++ docs/src/app/(home)/layout.tsx
@@ -1,16 +1,53 @@
 import type { ReactNode } from "react";
 import { HomeLayout } from "fumadocs-ui/layouts/home";
 import { baseOptions } from "@/app/layout.config";
+import {
+	NavigationMenu,
+	NavigationMenuContent,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import Link from "next/link";
 
 export default function Layout({ children }: { children: ReactNode }) {
 	return (
 		<HomeLayout
 			{...baseOptions}
 			links={[
-				{
+				/*{
 					text: "Documentation",
 					url: "/docs/app",
 					active: "nested-url",
+				},*/
+				{
+					type: "custom",
+					on: "nav",
+					children: (
+						<NavigationMenu>
+							<NavigationMenuList>
+								<NavigationMenuItem>
+									<NavigationMenuTrigger>
+										<Link href="/docs/app">Documentation</Link>
+									</NavigationMenuTrigger>
+									<NavigationMenuContent>
+										<div className="grid grid-cols-2 w-[300px] gap-2">
+											<NavigationMenuLink className="row-span-2" asChild>
+												<Link href="/docs/app">App</Link>
+											</NavigationMenuLink>
+											<NavigationMenuLink asChild>
+												<Link href="/docs/app/installation">Installation</Link>
+											</NavigationMenuLink>
+											<NavigationMenuLink asChild>
+												<Link href="/docs/modules">Modules</Link>
+											</NavigationMenuLink>
+										</div>
+									</NavigationMenuContent>
+								</NavigationMenuItem>
+							</NavigationMenuList>
+						</NavigationMenu>
+					),
 				},
 			]}
 		>
